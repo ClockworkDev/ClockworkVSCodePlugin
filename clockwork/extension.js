ded1df02bca9ef9c39c44d1911bee361ad623609
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2017-02-02 23:42:22 +0100

    readManifest, open the runtime on deploy
    (The actual deploy server logic not implemented yet)

--- clockwork/extension.js
+++ clockwork/extension.js
@@ -6,7 +6,7 @@ var exec = require('child_process').exec;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-
+    const serverPort = 3000;
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "clockwork" is now active!');
@@ -24,17 +24,48 @@ function activate(context) {
             });
         })
     });
+    context.subscriptions.push(disposable);
 
-    disposable = vscode.commands.registerCommand('extension.buildProject', function () {
+    function buildProject(callback) {
         var thisExtension = vscode.extensions.getExtension('arcadio.clockwork');
-        exec(thisExtension.extensionPath + "/node_modules/.bin/clockwork build ", { cwd: vscode.workspace.rootPath }, function (error, stdout, stderr) {
+        exec(thisExtension.extensionPath + "/node_modules/.bin/clockwork build ", { cwd: vscode.workspace.rootPath }, callback);
+    }
+
+    disposable =vscode.commands.registerCommand('extension.buildProject', function () {
+        buildProject(function (error, stdout, stderr) {
             if (!error) {
                 vscode.window.showInformationMessage(`Project built successfully`);
             }
         });
     });
-
     context.subscriptions.push(disposable);
+
+    disposable = vscode.commands.registerCommand('extension.deployPackage', function () {
+        var manifest = readManifest(); ``
+        if (manifest != null) {
+            buildProject(function (error, stdout, stderr) {
+                if (!error) {
+                    // localServer.setDeployPackage(vscode.workspace.rootPath + "/"+manifest.name+".cw");
+                    const opn = require('opn');
+                    opn("cwrt://" + serverPort + "/deployPackage");
+                }
+            });
+        }
+    });
+    context.subscriptions.push(disposable);
+    
+
+    function readManifest(safeMode) {
+        try {
+            var manifest = require(vscode.workspace.rootPath + '/manifest.json');
+        } catch (e) {
+            vscode.window.showErrorMessage("There is no Clockwork project in this folder! (manifest.json is missing)");
+            vscode.window.showErrorMessage("If you want to create a new Clockwork game, run 'Create Clockwork project'");
+            return null;
+        }
+        return manifest;
+    }
+
 
 }
 exports.activate = activate;
